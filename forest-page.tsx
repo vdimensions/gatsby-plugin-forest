03126a22fa84383c509fe1be594ba98dc02c7274
ivaylo5ev <ivaylo5ev@gmail.com>
2021-12-31 22:04:58 +0200

    - fixing recursion issue

--- forest-page.tsx
+++ forest-page.tsx
@@ -7,32 +7,34 @@ type GatsbyNavigatorProps = {
     template: string
 }
 
-const GatsbyNavigatorInner : React.FC<GatsbyNavigatorProps> = (props) => {
+const GatsbyNavigatorInner : React.FC<GatsbyNavigatorProps> = React.memo((props) => {
     console.debug("GatsbyNavigatorInner");
     const navigate = useNavigate();
     React.useEffect(() => {
         console.debug("GatsbyNavigator navigating to ", props.template);
         navigate(props.template);
-    });
+    }, [props.template]);
     
     return (<>{props.children}</>);
-}
+});
 
 const ForestPage = (props) => {
     const {template, data} = props.pageContext;
     
-    const GatsbyNavigator : React.FC<any> = React.memo(() => {
+    const GatsbyNavigator : React.FC<any> = React.memo((props) => {
 
         console.debug("GatsbyNavigator");
         return (
             <ForestHooksContext.Provider value={DefaultForestHooks}>
-                <GatsbyNavigatorInner template={template} />
+                <GatsbyNavigatorInner template={template}>
+                    {props.children}
+                </GatsbyNavigatorInner>
             </ForestHooksContext.Provider>
         );
     });
 
     console.debug("ForestPage");
-    
+
     return (
         <ForestApp 
             loadingIndicator={<span>LOADING</span>}
