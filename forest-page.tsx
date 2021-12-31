4c1e41e647587a7a38270dd1f92a10514ae72793
ivaylo5ev <ivaylo5ev@gmail.com>
2021-12-31 21:11:12 +0200

    - fixed issue with gatsby navigator

--- forest-page.tsx
+++ forest-page.tsx
@@ -1,22 +1,33 @@
 const React = require("react");
-import { ForestApp, useNavigate } from '@vdimensions/forest-js-react';
+import { DefaultForestHooks, ForestApp, ForestHooksContext, useNavigate } from '@vdimensions/forest-js-react';
 import { ForestGatsbyClient } from "./client";
 import "/src/forest/views";
 
+type GatsbyNavigatorProps = {
+    template: string
+}
 
+const GatsbyNavigatorInner : React.FC<GatsbyNavigatorProps> = (props) => {
+    const navigate = useNavigate();
+    React.useEffect(() => {
+        console.debug("GatsbyNavigator navigating to ", props.template);
+        navigate(props.template);
+    });
+    
+    return (<>{props.children}</>);
+}
 
 const ForestPage = (props) => {
     const {template, data} = props.pageContext;
     
-    const GatsbyNavigator : React.FC<any> = (props) => {
-        const navigate = useNavigate();
-        React.useEffect(() => {
-            console.debug("GatsbyNavigator navigating to ", template);
-            navigate(template);
-        });
-        
-        return (<>{props.children}</>);
+    const GatsbyNavigator : React.FC<any> = () => {
+        return (
+            <ForestHooksContext.Provider value={DefaultForestHooks}>
+                <GatsbyNavigatorInner template={template} />
+            </ForestHooksContext.Provider>
+        );
     }
+    
     return (
         <ForestApp 
             loadingIndicator={<span>LOADING</span>}
